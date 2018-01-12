6a3b9d569dda4805ee55b428ed5aa97f61091ba7
Nolan Lunscher <nlunscher@clearpath.ai>
2018-01-12 17:16:07 +0000

    args in launch, colorless pointclouds

--- realsense_ros_camera/src/base_realsense_node.cpp
+++ realsense_ros_camera/src/base_realsense_node.cpp
@@ -392,7 +392,7 @@ void BaseRealSenseNode::setupStreams()
                 publishFrame(frame, t);
             }
 
-            if(_pointcloud && is_depth_frame_arrived && is_color_frame_arrived &&
+            if(_pointcloud && is_depth_frame_arrived && //is_color_frame_arrived &&
                (0 != _pointcloud_publisher.getNumSubscribers()))
             {
                 ROS_DEBUG("publishPCTopic(...)");
@@ -853,27 +853,36 @@ void BaseRealSenseNode::publishPCTopic(const ros::Time& t)
             *iter_y = depth_point[1];
             *iter_z = depth_point[2];
 
-            rs2_transform_point_to_point(color_point, &_depth2color_extrinsics, depth_point);
-            rs2_project_point_to_pixel(color_pixel, &color_intrinsics, color_point);
-
-            if (color_pixel[1] < 0.f || color_pixel[1] > color_intrinsics.height
-                || color_pixel[0] < 0.f || color_pixel[0] > color_intrinsics.width)
+            if (_enable[COLOR])
             {
-                // For out of bounds color data, default to a shade of blue in order to visually distinguish holes.
-                // This color value is same as the librealsense out of bounds color value.
-                *iter_r = static_cast<uint8_t>(96);
-                *iter_g = static_cast<uint8_t>(157);
-                *iter_b = static_cast<uint8_t>(198);
+                rs2_transform_point_to_point(color_point, &_depth2color_extrinsics, depth_point);
+                rs2_project_point_to_pixel(color_pixel, &color_intrinsics, color_point);
+
+                if (color_pixel[1] < 0.f || color_pixel[1] > color_intrinsics.height
+                    || color_pixel[0] < 0.f || color_pixel[0] > color_intrinsics.width)
+                {
+                    // For out of bounds color data, default to a shade of blue in order to visually distinguish holes.
+                    // This color value is same as the librealsense out of bounds color value.
+                    *iter_r = static_cast<uint8_t>(96);
+                    *iter_g = static_cast<uint8_t>(157);
+                    *iter_b = static_cast<uint8_t>(198);
+                }
+                else
+                {
+                    auto i = static_cast<int>(color_pixel[0]);
+                    auto j = static_cast<int>(color_pixel[1]);
+
+                    auto offset = i * 3 + j * color_intrinsics.width * 3;
+                    *iter_r = static_cast<uint8_t>(color_data[offset]);
+                    *iter_g = static_cast<uint8_t>(color_data[offset + 1]);
+                    *iter_b = static_cast<uint8_t>(color_data[offset + 2]);
+                }
             }
             else
             {
-                auto i = static_cast<int>(color_pixel[0]);
-                auto j = static_cast<int>(color_pixel[1]);
-
-                auto offset = i * 3 + j * color_intrinsics.width * 3;
-                *iter_r = static_cast<uint8_t>(color_data[offset]);
-                *iter_g = static_cast<uint8_t>(color_data[offset + 1]);
-                *iter_b = static_cast<uint8_t>(color_data[offset + 2]);
+                *iter_r = static_cast<uint8_t>(96);
+                *iter_g = static_cast<uint8_t>(157);
+                *iter_b = static_cast<uint8_t>(198);
             }
 
             ++image_depth16;
@@ -951,6 +960,7 @@ void BaseRealSenseNode::publishFrame(rs2::frame f, const ros::Time& t)
     ++(_seq[stream]);
     auto& info_publisher = _info_publisher[stream];
     auto& image_publisher = _image_publishers[stream];
+
     if(0 != info_publisher.getNumSubscribers() ||
        0 != image_publisher.getNumSubscribers())
     {
@@ -967,6 +977,7 @@ void BaseRealSenseNode::publishFrame(rs2::frame f, const ros::Time& t)
 
         sensor_msgs::ImagePtr img;
         img = cv_bridge::CvImage(std_msgs::Header(), _encoding[stream], image).toImageMsg();
+
         img->width = width;
         img->height = height;
         img->is_bigendian = false;
@@ -979,8 +990,8 @@ void BaseRealSenseNode::publishFrame(rs2::frame f, const ros::Time& t)
         cam_info.header.stamp = t;
         cam_info.header.seq = _seq[stream];
         info_publisher.publish(cam_info);
-
         image_publisher.publish(img);
+
         ROS_DEBUG("%s stream published", rs2_stream_to_string(f.get_profile().stream_type()));
     }
 }
